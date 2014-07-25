c5f2ff80e0aa042f56072e1e47e8e83cf73d2259
MelonCoffeeStudios <aaronjgriffin96@gmail.com>
2014-07-25 20:55:31 +0100

    Clear function

--- js/typing.js
+++ js/typing.js
@@ -12,7 +12,7 @@ $(function(){
 });
 
 
-var stringArrayOne = ["Welcome to GriffTec Corp Terminal Control", "Please enter valid GriffTec username:", "root", "Please enter password:**********", ] // Intro strings
+var stringArrayOne = ["Welcome to GriffTec Corp Terminal Control", "Please enter valid GriffTec username:", "root", "Please enter password:","**********", "Password accepted! Welcome 'root'.", "Clearing terminal!"] // Intro strings
 var stringArrayTwo = ["temp"]
 
 
@@ -26,11 +26,11 @@ var cursorHTML = "<span class='cursorCSS'>&nbsp;</span>"
 
 function type(what, where, button, callafter){
 	//Clear Cursor
-	$(".cursorCSS").remove();
+	$(".cursorCSS").remove()
 
 	// Typing function
-	var d = new Date();
-    var n = d.getTime();
+	var d = new Date()
+    var n = d.getTime()
 	$("#terminalTextContainer").append("<div id='"+where+n+"Div'> </div>")
 	$("#"+where+n+"Div").append("<p id="+where+n+"></p>"+cursorHTML)
 	var i = 0                    //  set your counter to 1
@@ -39,12 +39,12 @@ function type(what, where, button, callafter){
 	function sleepFor () {           //  create a loop function
 	   setTimeout(function () {    //  call a 3s setTimeout when the loop is called
 	      $("#"+where+n).append(letter[i])         //  your code here
-	      i++;                     //  increment the counter
+	      i++                   //  increment the counter
 	      if (i < letter.length) {            //  if the counter < 10, call the loop function
 	         sleepFor()           //  ..  again which will trigger another 
 	      }  
 	      else{
-	      	$("#"+where+n+"Div").append("<br>");
+	      	$("#"+where+n+"Div").append("<br>")
 	      }                     //  ..  setTimeout()
 	   }, Math.floor((Math.random() * 100) + 5))
 	}
@@ -56,18 +56,41 @@ sleepFor();
 }
 
 
-
-setTimeout(function(){
-type(stringArrayOne[0], "lineOne")
+function timeoutType(what, where, button, when){
 	setTimeout(function(){
-		type(stringArrayOne[1], "lineTwo")
-	},3000)
-}, 1000)
-
-function intro(){
-	timeoutType(fs.one[0],"lineOne", 0, 1000);
-	timeoutType(fs.one[1],"lineTwo", 0, 3000);
-	timeoutType(fs.one[2],"lineThree",0,5500);
-	timeoutType(fs.one[3],"lineFour",0,7500);
+		type(what,where,button)
+	},when)
 }
 
+
+function intro(){
+	$("#terminalTextContainer").html("")
+
+	timeoutType(fs.one[0],"l1",0,1000)
+	timeoutType(fs.one[1],"l2",0,3500)
+	timeoutType(fs.one[2],"l3",0,6000)
+	timeoutType(fs.one[3],"l4",0,8000)
+	timeoutType(fs.one[4],"l5",0,10000)
+	timeoutType(fs.one[5],"l6",0,12500)
+	timeoutType(fs.one[6],"l7",0,14500)
+
+	setTimeout(function(){
+		clearTerminal();
+	},15000)
+}
+
+
+setTimeout(intro(), 750)
+
+function clearTerminal(){
+	var terminalText = $("#terminalTextContainer div").length
+	if(terminalText != 0){
+		setTimeout(function(){
+			$("#terminalTextContainer div:first-child").remove();
+			clearTerminal();
+		},100)
+	}
+	else{
+		
+	}
+}
